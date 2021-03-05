58d1a7079ceac8f65084df7c337299d68d082014
MittelDank <mitteldank@protonmail.com>
2021-03-05 00:00:15 -0800

    session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,34 @@
 class SessionsController < ApplicationController
+
   def new
+    @meta_tag= "noindex"
   end
 
   def create
+    if check_pin()
+      @user = User.find_by_username(params[:session][:username])
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        # User.new(username: #user info here
+
+      end
+    else 
+      redirect_to new_session_path
+      flash[:alert] = "Oops.. Houston, we have a problem..."
+    end
   end
 
   def destroy
+    @meta_tag= "noindex"
+
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def check_pin
+
   end
 
 end
